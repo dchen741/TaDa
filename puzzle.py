0814916c1440285e5e441c9c054bdfb5ee1c2bc4
Andrew Mendelsohn <andrew.mendelsohn@tufts.edu>
2014-12-01 19:51:17 -0500

    fixed bug in puzzle

--- puzzle.py
+++ puzzle.py
@@ -1,23 +1,31 @@
 import text_interface as interface
 
+debug = False
+
 class Puzzle:
-	def __init__(self, title, lesson, prompt, code, answer):
-		self.id = 1
+	def __init__(self, title, lesson, prompt, code, answer, hint=''):
+		self.id 		= 1 #TODO: increment ids
 		self.solved = False
-		self.title = title
+		self.title 	= title
 		self.lesson = lesson
-		self.prompt = prompt
-		self.code = code
-		self.answer = answer
+		self.prompt	= prompt
+		self.code 	= code
+		self.answer	= answer
+		self.hint 	= hint
 
 	def start_puzzle(self):
 		while(not self.solved):
 			interface.put_text(self.lesson)
-			interface.get_text('Ready?\n')
+			interface.get_text('Ready for the puzzle? (y/n)\n')
 			interface.put_text(self.code)
 			userInput = None
 			while(True):
 				userInput = interface.get_text(self.prompt)
+				if debug:
+					print 'expected: '
+					print self.answer
+					print 'and  got: '
+					print self.process_input(userInput)
 				if(self.process_input(userInput) == self.answer):
 					interface.put_text("Congratulations! That's correct. Good job!\n")
 					break
--- test_puzzle.py
+++ test_puzzle.py
@@ -5,7 +5,7 @@ def main():
   p = puzzle.Puzzle("Hello World Puzzle",
                     "This bit explains the concepts.",
                     "Where would you put 'print 'hello world!'?",
-                    "1 print 'ahoy'\n2 print 'yahoo'",
+                    "print 'ahoy'\nprint 'yahoo'",
                     "print 'ahoy'\nprint 'hello world!'\nprint 'yahoo'",
                     )
   p.start_puzzle()
