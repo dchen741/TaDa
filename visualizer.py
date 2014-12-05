0329f7bfdf67b8ba6a83b8999697668954acaf7f
Ali Tai <alictai@gmail.com>
2014-12-05 18:09:58 -0500

    edited puzzle

--- visualizer.py
+++ visualizer.py
@@ -34,33 +34,5 @@ def mk_thread(s):
     exec s in globals(), locals()
   return f
 
-# def run_code():
-#   str1 = [ 'for x in xrange(100):'
-#          , '     import time'
-#          , '     time.sleep(0.01)'
-#          , '     controller.update_state(thread_id, str(x))'
-#          , 'controller.update_state(thread_id, "complete")']
-#   str2 = [ 'for x in xrange(100):'
-#          , '     import time'
-#          , '     time.sleep(0.01)'
-#          , '     controller.update_state(thread_id, str(x))'
-#          , 'controller.update_state(thread_id, "complete")']
-#   code = ['\n'.join(str1), '\n'.join(str2)]
-#   threads = map(mk_thread, code)
-
-#   def pred(states):
-#     global statesList
-#     #  print 'State 0: %i, State 1: %i' % (states[0], states[1])
-#     if states[0] == "complete" and states[1] == "complete":
-#       sim.add_state((states[0], states[1]))
-#       return True, None
-#     else:
-#       sim.add_state((states[0], states[1]))
-#       return False, None
-
-#   sim = simulator.Simulator(threads, pred)
-#   sim.run_sim()
-#   sim.visualize()
-
 if __name__ == '__main__':
   main()
